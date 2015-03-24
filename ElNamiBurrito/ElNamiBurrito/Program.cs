f94488591483564007adb19b1271d8cc4a955fe7
jQuery <info@zavox.nl>
2015-03-24 19:39:03 +0100

    nami

---
+++ ElNamiBurrito/ElNamiBurrito/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace ElNamiBurrito
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Nami.Game_OnGameLoad;
+        }
+    }
+}
