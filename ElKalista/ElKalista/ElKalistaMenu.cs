cae76d1b49629f390543255485c32df21232d8d7
jQuery <info@zavox.nl>
2015-05-07 06:29:49 +0200

    Kalista

--- ElKalista/ElKalista/ElKalistaMenu.cs
+++ ElKalista/ElKalista/ElKalistaMenu.cs
@@ -126,6 +126,7 @@ namespace ElKalista
             _menu.AddSubMenu(credits);
 
             _menu.AddItem(new MenuItem("422442fsaafs4242f", ""));
+            _menu.AddItem(new MenuItem("422442fsaafsf", "Version: 1.0.2.2"));
             _menu.AddItem(new MenuItem("fsasfafsfsafsa", "Made By jQuery"));
 
             _menu.AddToMainMenu();
