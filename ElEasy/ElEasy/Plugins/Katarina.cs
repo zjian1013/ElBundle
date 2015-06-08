d90ddeee43609ef17453261d68bb693150949a65
jQuery <nathan@zavox.nl>
2015-06-08 11:23:20 +0200

    Update Katarina.cs

--- ElEasy/ElEasy/Plugins/Katarina.cs
+++ ElEasy/ElEasy/Plugins/Katarina.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -142,6 +142,29 @@ namespace ElEasy.Plugins
             var wardjump = _menu.Item("ElEasy.Katarina.Wardjump").GetValue<KeyBind>().Active;
             if (wardjump)
                 DoWardJump();
+        
+            var autor = new int[] { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 };            
+            int qOff = 0, wOff = 0, eOff = 0, rOff = 0;
+
+            int qL = Player.Spellbook.GetSpell(SpellSlot.Q).Level + qOff;
+            int wL = Player.Spellbook.GetSpell(SpellSlot.W).Level + wOff;
+            int eL = Player.Spellbook.GetSpell(SpellSlot.E).Level + eOff;
+            int rL = Player.Spellbook.GetSpell(SpellSlot.R).Level + rOff;
+            if (qL + wL + eL + rL < ObjectManager.Player.Level)
+            {
+                int[] level = new int[] { 0, 0, 0, 0 };
+                for (int i = 0; i < ObjectManager.Player.Level; i++)
+                {
+                    level[autor[i] - 1] = level[autor[i] - 1] + 1;
+               }
+               if (qL < level[0]) ObjectManager.Player.Spellbook.LevelSpell(SpellSlot.Q);
+                if (wL < level[1]) ObjectManager.Player.Spellbook.LevelSpell(SpellSlot.W);
+                if (eL < level[2]) ObjectManager.Player.Spellbook.LevelSpell(SpellSlot.E);
+                if (rL < level[3]) ObjectManager.Player.Spellbook.LevelSpell(SpellSlot.R);
+
+            }
+        }
+ 
         }
 
         #endregion
