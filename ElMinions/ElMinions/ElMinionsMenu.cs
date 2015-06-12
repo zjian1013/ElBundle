eeb644cb95adbeee4f3465b04be86c5bdc34ecf0
jQuery <info@zavox.nl>
2015-06-12 04:57:40 +0200

    kek nigga

---
+++ ElMinions/ElMinions/ElMinionsMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.Drawing;
+
+namespace ElMinions
+{
+    public static class ElMinionsMenu
+    {
+        public static Menu Menu;
+
+        public static void Initialize()
+        {
+            Menu = new Menu("ElMinions", "menu", true);
+
+            var drawMenu = new Menu("Drawings", "Drawings");
+            drawMenu.AddItem(new MenuItem("ElMinions.Draw.Minions", "Draw minions").SetValue(true));
+            Menu.AddSubMenu(drawMenu);
+
+            Menu.AddToMainMenu();
+        }
+    }
+}
---
+++ ElMinions/ElMinions/Main.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+
+
+namespace ElMinions
+{
+    internal static class Main
+    {
+        #region OnLoad
+
+        public static void OnLoad(EventArgs args)
+        {
+            Notifications.AddNotification("ElMinions", 10000);
+            ElMinionsMenu.Initialize();
+            Drawing.OnEndScene += OnEndScene;
+        }
+
+        #endregion
+
+        #region OnEndScene
+
+        private static void OnEndScene(EventArgs args)
+        {
+            var isActive = ElMinionsMenu.Menu.Item("ElMinions.Draw.Minions").GetValue<bool>();
+
+            if (isActive)
+            {
+                // blabla
+            }
+        }
+
+        #endregion
+    }
+}
